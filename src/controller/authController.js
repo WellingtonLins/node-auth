b54aedcef61359b2e1d9ce7975e6f84bc15a4e4b
wellingtonlins <wellingtonlins2013@gmail.com>
2019-08-27 12:09:58 -0300

    verifying if email already exists

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -8,13 +8,13 @@ router.post("/register", async (req, res) => {
   const { email } = req.body;
 
   try {
-    // if (await User.findOne({ email })) {
-    //   return res.sendStatus(400).send({ error: "User already exists" });
-    // } else {
+    if (await User.findOne({ email })) {
+      return res.status(400).send({ error: "User already exists" });
+    } else {
       const user = await User.create(req.body);
       user.password = undefined;
       return res.send({ user });
-    // }
+    }
   } catch (err) {
     return res.status(400).send({ error: "Registration failed" });
   }
