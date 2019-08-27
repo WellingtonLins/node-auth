480ca1a5e670b75a4d61147990ad3c4e041b988f
wellingtonlins <wellingtonlins2013@gmail.com>
2019-08-27 11:57:22 -0300

    ok

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -5,9 +5,16 @@ const User = require("../models/user");
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
+  const { email } = req.body;
+
   try {
-    const user = await User.create(req.body);
-    return res.send({ user });
+    // if (await User.findOne({ email })) {
+    //   return res.sendStatus(400).send({ error: "User already exists" });
+    // } else {
+      const user = await User.create(req.body);
+      user.password = undefined;
+      return res.send({ user });
+    // }
   } catch (err) {
     return res.status(400).send({ error: "Registration failed" });
   }
